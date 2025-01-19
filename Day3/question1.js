0d97822142a41dfd390e9a07f0a1f8641bddc359
Simran8485 <119832214+Simran8485@users.noreply.github.com>
2025-01-19 16:55:18 +0530

    Add files via upload

---
+++ Day3/question1.js
@@ -0,0 +1,7 @@
+function calculateDiscount(price, discount = 10) {
+    return price - (price * discount) / 100;
+}
+
+
+console.log(calculateDiscount(100)); 
+console.log(calculateDiscount(100, 20)); 
---
+++ Day3/question3.js
@@ -0,0 +1,6 @@
+function logArguments(...args) {
+    console.log(args);
+}
+
+
+logArguments(1, 'hello', true, { key: 'value' }); 
